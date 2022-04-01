49a83eba34cff5af46619bcd800f19ad66949acb
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-04-01 17:18:03 +0200

    Implement NodeProxy

--- src/com/lechowicz/NodeProxy.java
+++ src/com/lechowicz/NodeProxy.java
@@ -3,23 +3,27 @@ package com.lechowicz;
 public class NodeProxy implements INode{
     protected INode element;
 
+    public NodeProxy(INode element) {
+        this.element = element;
+    }
+
     @Override
     public String getName() {
-        return null;
+        return this.element.getName();
     }
 
     @Override
     public void setName(String name) {
-
+        this.element.setName(name);
     }
 
     @Override
     public INode getParent() {
-        return null;
+        return this.element.getParent();
     }
 
     @Override
     public void setParent(INode parent) {
-
+        this.element.setParent(parent);
     }
 }
