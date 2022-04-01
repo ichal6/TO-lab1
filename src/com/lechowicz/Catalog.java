3b80f93acc4cd659cc0c6ebdb85fd820abf66759
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-04-01 17:17:42 +0200

    Implement Catalog

--- src/com/lechowicz/Catalog.java
+++ src/com/lechowicz/Catalog.java
@@ -1,47 +1,71 @@
 package com.lechowicz;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Catalog extends Node{
     protected List<INode> list;
 
+    public Catalog(String name, INode parent) {
+        this.list = new LinkedList<>();
+        this.parent = parent;
+        this.name = name;
+    }
+
     public INode getElement(INode el){
-        return null;
+        int index = this.list.indexOf(el);
+        if(index != -1){
+            return this.list.get(index);
+        } else {
+            return null;
+        }
     }
 
-    public List<INode> getAllElement(){
-        return null;
+    public List<INode> getAllElements(){
+        return this.list;
     }
 
-    public void setElement(INode el){
+    public void setElement(INode oldEl, INode newEl){
+        for (INode exElem: this.list) {
+            if(exElem.getName().equals(newEl.getName()))
+                return;
+        }
 
+        int index = this.list.indexOf(oldEl);
+        if (index != -1) {
+            this.list.set(index, newEl);
+        }
     }
 
     public void remElement(INode el){
-
+        this.list.remove(el);
     }
 
     public void addElement(INode el){
-
+        for (INode exElem: this.list) {
+            if(exElem.getName().equals(el.getName()))
+                return;
+        }
+        this.list.add(el);
     }
 
     @Override
     public String getName() {
-        return null;
+        return this.name;
     }
 
     @Override
     public void setName(String name) {
-
+        this.name = name;
     }
 
     @Override
     public INode getParent() {
-        return null;
+        return this.parent;
     }
 
     @Override
     public void setParent(INode parent) {
-
+        this.parent = parent;
     }
 }
