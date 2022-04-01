d80c5b026ae188afaedac54ab58afb12f9c7c2f6
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-04-01 15:12:14 +0200

    Add inheritance

--- src/com/lechowicz/File.java
+++ src/com/lechowicz/File.java
@@ -1,13 +1,33 @@
 package com.lechowicz;
 
-public class File {
-    private byte[] content;
+public class File extends Node{
+    protected byte[] content;
 
     public byte[] getContent(){
         return null;
     }
 
     public void setContent(byte[] con) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public INode getParent() {
+        return null;
+    }
+
+    @Override
+    public void setParent(INode parent) {
 
     }
 }
