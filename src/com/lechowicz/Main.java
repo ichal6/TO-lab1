275c9210b6154d5b1b1b092f5f0a511e74b7d1e9
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-04-01 17:18:17 +0200

    Write main file

--- src/com/lechowicz/Main.java
+++ src/com/lechowicz/Main.java
@@ -3,6 +3,39 @@ package com.lechowicz;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        Catalog listOfSubject = new Catalog("List of subject", null);
+
+        Catalog term6 = new Catalog("Term 6", listOfSubject);
+
+        Catalog term5 = new Catalog("Term 5", listOfSubject);
+
+        Catalog TO = new Catalog("Technologie Obiektowe", term6);
+
+        Catalog JS = new Catalog("Języki Symboliczne", term5);
+
+        File noteLab1 = new File("Note", TO, "Notatka z zajęć 20.03.2022".getBytes());
+
+        File noteLab2 = new File("Note", TO, "Notatka z zajęć 20.03.2022".getBytes());
+
+        File noteLab3 = new File("Note", JS, "Notaka z poprzedniego semstru".getBytes());
+
+        TO.addElement(noteLab1);
+        TO.addElement(noteLab2);
+        JS.addElement(noteLab3);
+
+        System.out.println(JS.getElement(noteLab3).getName());
+        NodeProxy nodeProxy1 = new NodeProxy(noteLab1);
+
+        nodeProxy1.setName("Inny");
+
+        nodeProxy1.setParent(term5);
+
+        NodeProxy nodeProxy2 = new NodeProxy(JS);
+
+
+        System.out.println(JS.getName());
+        nodeProxy2.setName("JS");
+        System.out.println(nodeProxy2.getName());
+        System.out.println(JS.getName());
     }
 }
