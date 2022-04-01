0fe9c116c3334bd779ed9a838b06ef48875a01db
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-04-01 17:17:52 +0200

    Implement File

--- src/com/lechowicz/File.java
+++ src/com/lechowicz/File.java
@@ -3,31 +3,37 @@ package com.lechowicz;
 public class File extends Node{
     protected byte[] content;
 
-    public byte[] getContent(){
-        return null;
+    public File(String name, INode parent, byte[] content) {
+        this.content = content;
+        this.parent = parent;
+        this.name = name;
+    }
+
+    public byte[] getContent() {
+        return this.content;
     }
 
     public void setContent(byte[] con) {
-
+        this.content = con;
     }
 
     @Override
     public String getName() {
-        return null;
+        return this.name;
     }
 
     @Override
     public void setName(String name) {
-
+        this.name = name;
     }
 
     @Override
     public INode getParent() {
-        return null;
+        return this.parent;
     }
 
     @Override
     public void setParent(INode parent) {
-
+        this.parent = parent;
     }
 }
